6599b4e5ef0b8f2c47bd835f2cac73a83e9404c4
Michal <numlok3@o2.pl>
2020-10-18 10:54:48 +0200

    Reworked calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,65 +10,46 @@
 //        - pomnoz liczbe przez wartosci pi(2 miejsca po przecinku)
 //        - podziel liczbe przez wartosci pi(2 miejsca po przecinku)
 
-import java.nio.channels.Pipe;
+
 import java.util.Scanner;
 
+/**
+ * Do math tasks.
+ */
 public class Calculator {
-    public static void main(String[] args) {
 
-        Scanner dupa = new Scanner(System.in);
-
-        double number1;
-        double number2;
-        double a = 3.14;
-
-        System.out.println("write 1st number");
-        number1 = dupa.nextDouble();
-        System.out.println("write 2nd number");
-        number2 = dupa.nextDouble();
-
-        Addition(number1, number2);
-        Substraction(number1, number2);
-        Multipication(number1, number2);
-        Division(number1, number2);
-        PiAddition(number1, a);
-        PiSubstraction(number1, a);
-        PiMultiplication(number1, a);
-        PiDivision(number1, a);
-    }
+    private static final double PI = 3.14;
 
     //methods
-    static void Addition(double number1, double number2) {
-        System.out.println("addition = " + (number1 + number2));
+    double add(double number1, double number2) {
+        return number1 + number2;
     }
 
-    static void Substraction(double number1, double number2) {
-        System.out.println("substraction = " + (number1 - number2));
+    double subtract(double number1, double number2) {
+        return number1 - number2;
     }
 
-    static void Multipication(double number1, double number2) {
-        System.out.println("multipication = " + (number1 * number2));
+    double multiply(double number1, double number2) {
+        return number1 * number2;
     }
 
-    static void Division(double number1, double number2) {
-        System.out.println("division = " + (number1 / number2));
+    int divide(int number1, int number2) {
+        return number1 / number2;
     }
 
-    static void PiAddition(double number1, double a) {
-
-        System.out.println("pi addition = " + (number1 + a));
+    double addByPi(double number1) {
+        return number1 + PI;
     }
 
-    static void PiSubstraction(double number1, double a) {
-
-        System.out.println("pi substraction = " + (number1 - a));
+    double subtractByPi(double number1) {
+        return number1 - PI;
     }
 
-    static void PiMultiplication(double number1, double a) {
-        System.out.println("pi multiplication = " + (number1 * a));
+    double multiplyByPi(double number1) {
+        return number1 * PI;
     }
 
-    static void PiDivision(double number1, double a) {
-        System.out.println("pi division = " + (number1 / a));
+    double divideByPi(double number1) {
+        return number1 / PI;
     }
 }
